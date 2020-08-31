5393d0490d02856ad3d8c1115649345b0d84b9df
sofia SVF <sofia.gonzales@live.com.ar>
2020-08-31 20:44:36 -0300

    suba de ejercicio 2

---
+++ ejercicio_dos/index.js
@@ -0,0 +1,2 @@
+var meses = ["enero", " febrero", " marzo", " abril", " mayo", " junio", " julio", " agosto", " septiembre", " octubre", " noviembre", " diciembre"];
+console.log("Los meses del año son: " + meses);
