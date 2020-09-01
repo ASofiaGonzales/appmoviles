fe8ad2f993820ce8c133a860ae4107888fc649ee
sofia SVF <sofia.gonzales@live.com.ar>
2020-08-31 22:17:40 -0300

    ejercicio 3 y 4

---
+++ ejercicio_tres/index.js
@@ -0,0 +1,16 @@
+
+var numero; 
+
+function parImpar (numero)
+{
+    if ( numero % 2 == 0)
+    {
+      return "Par";
+    }
+    else
+    {
+        return "Impar";
+    }
+}
+console.log(parImpar(89));
+ 
